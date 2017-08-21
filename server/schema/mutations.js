027b4474faa90686bb32af56a42ab7cad220bfd1
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-08-20 21:52:55 -0700

    add login mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -22,11 +22,29 @@ const mutation = new GraphQLObjectType({
       }
     },
     logout: {
-      type: UserType,
+      type: userType,
       resolve(parentValue, args, req) {
         const {user} = req;
         reg.logout();
         return user;
+      }
+    },
+    login: {
+      type: userType,
+      args: {
+        email: {
+          type: GraphQLString
+        },
+        password: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, {email, password}, req) {
+        return AuthService.login({
+          email,
+          password,
+          req
+        });
       }
     }
   }
