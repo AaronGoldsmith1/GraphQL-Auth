40e340b1f2a0e44b53e5004dec98da6cbca8ac9d
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-08-26 23:58:51 -0700

    build out auth form

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 class AuthForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
   render() {
     return (
-      <form></form>
+      <div className="row">
+        <form className="col s4">
+          <div className="input-field">
+            <label>Email</label>
+            <input
+      value={this.state.email}
+      onChange={e => this.setState({
+        email: e.target.value
+      })}
+      />
+              <div className="input-field">
+            <label>Password</label>
+            <input />
+              </div>
+          </div>
+        </form>
+      </div>
+
     )
   }
 }
