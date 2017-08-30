0243aa23214faf53e3045e7c45a14464c7e5d2dd
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-08-29 22:56:04 -0700

    add signup mutation

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import AuthForm from './AuthForm';
 
 class SignupForm extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <h3>Sign Up</h3>
+        <AuthForm />
+      </div>
     )
   }
 }
